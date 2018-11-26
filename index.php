f3bc5cccd03179135dcb8f50af4f435f5e27f559
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-11-26 15:46:31 -0500

    created header.php

--- index.php
+++ index.php
@@ -1,44 +1,4 @@
-
-<!doctype html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?> >
-
-<header>
-	<h1><a href="<?php echo home_url('/') ?>">
-		<?php bloginfo("name"); ?>
-		</a></h1>
-	<?php $sample = get_bloginfo("description") ?>
-	<?php if( is_front_page() && !is_home() ): ?>
-		<p><?php echo $sample ?></p>
-	<?php endif; ?>
-</header>
-<nav id="site-navigation" class="main-navigation" role="navigation">
-	<?php wp_nav_menu([
-		'theme_location' => 'central-menu'
-		
-	])  ?>
-</nav>
-
-<!-- end header.php? -->
-
-<div id="main" class="main">
-<!-- DISPLAYS TEMPLATE PAGE For trouble-shooting? -->
-<!-- 
-
-	<div id="output">
-	<?php 
-	//  if(is_index) echo "index.php";
-	 ?>
-</div>
--->
-
-<!-- END WP HEADER -->
+<?php get_header(); ?>
 
 <div class="primary">
 <?php 
