71ae09e22252f37b729146582447c7cbe1076898
Daniel Mulvihill <dmulvihill3@gmail.com>
2018-11-26 13:05:40 -0500

    fixed stuff

--- functions.php
+++ functions.php
@@ -4,12 +4,12 @@ add_theme_support("title-tag");
 add_theme_support("post-thumbnails");
 
 add_theme_support( "custom-background" );
-add_theme_support( "custom-header" );
+// add_theme_support( "custom-header" );
 add_theme_support( "custom-logo" );
 add_theme_support( "customize-selective-refresh-widgets" );
 
 function yak_enqueue_styles(){
-	wp_enqueue_style('main-css', get_stylesheet_directory_uri().'/style.css', [], time(), 'all' );
+	wp_enqueue_style('main-css', get_stylesheet_directory_uri().'/style.css', [], 'all' );
     wp_enqueue_script( 'script-js', get_template_directory_uri() . '/main.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', "yak_enqueue_styles");
